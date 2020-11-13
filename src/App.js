733e81b6ad80862d4b95b6caf2e8ab16b66e914f
abhishekBhayye <bhayyeabhishek@gmail.com>
2020-11-13 02:45:57 +0000

    Header for the Amazon clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Whats up</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header/>
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          {/* This is a default route */}
+          <Route path="/">
+            <Header/>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
